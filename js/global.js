8aade12f91d033cfc274b5220873013939295c45
Nathan Carpenter <nathan.carpenter03@gmail.com>
2017-03-01 20:49:43 -0500

    Pulling data from API, but not returning search results yet

--- js/global.js
+++ js/global.js
@@ -1,62 +1,49 @@
-createCard({
-    src: "http://unsplash.it/400/300?random",
-    caption: 'A picture of something'
-});
+// Declare empty items array
+var items= [];
 
-createCard({
-    src: "http://unsplash.it/400/300?random",
-    caption: 'A picture of something'
-});
+getItems();
 
-createCard({
-    src: "http://unsplash.it/400/300?random",
-    caption: 'A picture of something'
-});
-
-createCard({
-    src: "http://unsplash.it/400/300?random",
-    caption: 'A picture of something'
-});
-
-createCard({
-    src: "http://unsplash.it/400/300?random",
-    caption: 'A picture of something'
-});
-
-createCard({
-    src: "http://unsplash.it/400/300?random",
-    caption: 'A picture of something'
-});
-
-createCard({
-    src: "http://unsplash.it/400/300?random",
-    caption: 'A picture of something'
-});
-
-createCard({
-    src: "http://unsplash.it/400/300?random",
-    caption: 'A picture of something'
-});
-
-createCard({
-    src: "http://unsplash.it/400/300?random",
-    caption: 'A picture of something'
-});
-
-createCard({
-    src: "http://unsplash.it/400/300?random",
-    caption: 'A picture of something'
-});
-
-createCard({
-    src: "http://unsplash.it/400/300?random",
-    caption: 'A picture of something'
-});
-
-function createCard(image) {
-    var card = `<div class = "card col-sm-3">
-      <img src = "${image.src}" alt ="image" />
-      <h5 class = "card-caption">${image.caption}</h5>`;
-    document.querySelector('#app').innerHTML += card;
+// AJAX request- fetch etsy data - puts into new array called data
+function getItems() {
+    fetch('https://thinksaydo.com/tiyproxy.php?https://openapi.etsy.com/v2/listings/active?api_key=h9oq2yf3twf4ziejn10b717i&keywords=' + encodeURIComponent('dogs') + '&includes=Images,Shop')
+        .then(response => response.json())
+        .then(data => {
+            items = data.results;
+            items.forEach(function(item) {
+                createCard(item);
+        })
+    });
 }
+
+//  Function to create card with new data
+function createCard(item) {
+    var card = `<div class = "card col-sm-3">
+      <img src = "${item.Images[0].url_170x135}" alt ="image" />
+      <h5 class = "card-caption">${item.title.slice(0, 22)}...</h5>
+      <span class = "text-muted">${item.Shop.shop_name > 10 ? 
+          item.Shop.shop_name.slice(0,10) + '...' : item.Shop.shop_name}</span>
+                        <span class="text-muted pull-right">${item.price}</span>`;
+    document.querySelector('#card').innerHTML += card;
+}
+
+//---------------------------------------------------------------------------
+
+
+// Search button results- work in progress
+
+// var searchButton = document.querySelector('#searchButton');
+// var searchInput = '';
+
+// searchButton.addEventListener('click', function(e) {
+//     searchInput = document.querySelector('#searchInput').value;
+//     console.log(searchInput);
+//     fetch(`https://thinksaydo.com/tiyproxy.php?https://openapi.etsy.com/v2/listings/active?api_key=h9oq2yf3twf4ziejn10b717i&keywords=' + encodeURIComponent('${searchInput}') + '&includes=Images,Shop`)
+//         .then(response => response.json())
+//         .then(data => {
+//             items = data.results;
+//             items.forEach(function(item) {
+//                 createCard(item);
+//         })
+//     });
+// })
 
