dfd5cd00870d1e54343a9f82bdc5480645a8edfe
Nathan Carpenter <nathan.carpenter03@gmail.com>
2017-03-12 14:53:24 -0400

    fixed search results display

--- js/global.js
+++ js/global.js
@@ -1,21 +1,23 @@
-// Declare empty items array
-var items= [];
+// Search button results 
 
-getItems();
+var searchButton = document.querySelector('#searchButton');
 
-// AJAX request- fetch etsy data - puts into new array called data
-function getItems() {
-    fetch('https://thinksaydo.com/tiyproxy.php?https://openapi.etsy.com/v2/listings/active?api_key=h9oq2yf3twf4ziejn10b717i&keywords=' + encodeURIComponent('dogs') + '&includes=Images,Shop')
+searchButton.addEventListener('click', function() {
+    var searchInput = document.querySelector('#searchInput').value;
+    console.log(searchInput);
+    fetch('https://thinksaydo.com/tiyproxy.php?https://openapi.etsy.com/v2/listings/active?api_key=h9oq2yf3twf4ziejn10b717i&keywords=' + encodeURIComponent(searchInput) + '&includes=Images,Shop')
         .then(response => response.json())
         .then(data => {
-            items = data.results;
-            items.forEach(function(item) {
+            console.log(data);
+            // data.results.pop();
+            document.querySelector('#card').innerHTML = '';
+            data.results.forEach(function(item) {
                 createCard(item);
         })
     });
-}
+})
 
-//  Function to create card with new data
+//  Function to create card with pulled data
 function createCard(item) {
     var card = `<div class = "card col-sm-3">
       <img src = "${item.Images[0].url_170x135}" alt ="image" />
@@ -25,25 +27,3 @@ function createCard(item) {
                         <span class="text-muted pull-right">${item.price}</span>`;
     document.querySelector('#card').innerHTML += card;
 }
-
-//---------------------------------------------------------------------------
-
-
-// Search button results- work in progress
-
-// var searchButton = document.querySelector('#searchButton');
-// var searchInput = '';
-
-// searchButton.addEventListener('click', function(e) {
-//     searchInput = document.querySelector('#searchInput').value;
-//     console.log(searchInput);
-//     fetch(`https://thinksaydo.com/tiyproxy.php?https://openapi.etsy.com/v2/listings/active?api_key=h9oq2yf3twf4ziejn10b717i&keywords=' + encodeURIComponent('${searchInput}') + '&includes=Images,Shop`)
-//         .then(response => response.json())
-//         .then(data => {
-//             items = data.results;
-//             items.forEach(function(item) {
-//                 createCard(item);
-//         })
-//     });
-// })
-
